ed86883d1fe17748657ebaeb465004708a75031a
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2020-09-18 09:06:28 -0400

    validation token testing success

--- spec/requests/token_validation_spec.rb
+++ spec/requests/token_validation_spec.rb
@@ -5,16 +5,16 @@ RSpec.describe "Token Validations", type: :request do
     let(:user) { create(:user) }
     sign_in(:user)
 
-    # it 'should respond with success' do
-    #   get '/auth/validate_token' # yes, nothing changed here
-    #   expect(response).to have_http_status(:success)
-    # end
+    it 'should respond with success' do
+      get '/auth/validate_token' # yes, nothing changed here
+      expect(response).to have_http_status(:success)
+    end
   end
 
   describe 'signed out' do
-    # it 'should respond with unauthorized' do
-    #   get '/auth/validate_token'
-    #   expect(response).to have_http_status(:unauthorized)
-    # end
+    it 'should respond with unauthorized' do
+      get '/auth/validate_token'
+      expect(response).to have_http_status(:unauthorized)
+    end
   end
 end
