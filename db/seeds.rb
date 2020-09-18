ab6c9c4c4653890156406be1defc5a43eb7f0169
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2020-09-18 09:26:35 -0400

    seeders for roles created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,27 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE users_roles")
 Role.destroy_all
 User.destroy_all
 
-super_admin_role = Role.create(name: "super-admin")
-admin_role = Role.create(name: "admin")
-employee_role = Role.create(name: "employee")
-dispatcher_role = Role.create(name: "dispatcher")
-delivery_man_role = Role.create(name: "delivery-man")
-client_role = Role.create(name: "client")
+Role.create(name: "super-admin")
+Role.create(name: "admin")
+Role.create(name: "employee")
+Role.create(name: "dispatcher")
+Role.create(name: "delivery-man")
+Role.create(name: "client")
 
-super_admin = User.create(first_name: "Root", last_name: "User", email: "superadmin@htdevs.com", phone: "+571584455241", )
-admin = User.create(first_name: "Admin", last_name: "User", email: "admin@htdevs.com", phone: "+5715884455241", )
+super_admin = User.create(first_name: "Root", last_name: "User", email: "superadmin@htdevs.com", password: "password", phone: "+571584455241", )
+super_admin.add_role "super-admin"
 
-admin.add_role :admin
-# super_admin.add_role super_admin
-# admin.add_role admin
 
 case Rails.env
 when 'development'
-  # development-specific seeds ...
-  # (anything you need to interactively play around with in the rails console)
+
+  admin = User.create(first_name: "Admin", last_name: "User", email: "admin@htdevs.com", password: "password", phone: "+5715884455241", )
+  admin.add_role "admin"
+
+  employee = User.create(first_name: "Employee", last_name: "User", email: "employee@htdevs.com", password: "password", phone: "+5715884455241", )
+  employee.add_role "employee"
+
+  dispatcher = User.create(first_name: "Dispatcher", last_name: "User", email: "dispatcher@htdevs.com", password: "password", phone: "+5715884455241", )
+  dispatcher.add_role "dispatcher"
+
+  delivery_man = User.create(first_name: "Delivery-man", last_name: "User", email: "delivery-man@htdevs.com", password: "password", phone: "+5715884455241", )
+  delivery_man.add_role "delivery-man"
+
+  client = User.create(first_name: "Client", last_name: "User", email: "client@htdevs.com", password: "password", phone: "+5715884455241", )
+  client.add_role "client"
+
 
 when 'test'
   # test-specific seeds ...
