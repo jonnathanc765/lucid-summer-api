0c3fc5b609e30216edfce54a3a7788edcc7b1a49
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2020-09-23 19:51:02 -0400

    add cart validation and test

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,7 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_lines
+
+
+  validates :user_id, presence: true
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe "validate fields" do
+      it "should validate all fields" do
+        should validate_presence_of(:user_id)
+      end
+      
+  end
+  
 end
