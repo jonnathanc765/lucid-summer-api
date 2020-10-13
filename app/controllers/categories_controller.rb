5c94216fd141efc04ebbd415491b2988ff6f0d1b
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2020-10-13 14:05:38 -0400

    sort code

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,11 +9,14 @@ class CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
+
     if params[:parent_category].present?
       parent_category = Category.where(id: params[:parent_category].to_i)[0]
+
       if parent_category.nil?
         return render json: {"message" => "Parent category must exists ship must be exists"}.to_json, status: :unprocessable_entity
       end
+      
       @category.parent_category = parent_category
     end
     @category.save!
@@ -22,14 +25,19 @@ class CategoriesController < ApplicationController
 
   def update
     if params[:parent_category].present?
+
       parent_category = Category.where(id: params[:parent_category].to_i)[0]
+
       if parent_category.nil?
         return render json: {"message" => "Parent category must exists ship must be exists"}.to_json, status: :unprocessable_entity
       end
+
       if parent_category.id == @category.id
         return render json: {"message" => "Parent category can't be the same category"}.to_json, status: :unprocessable_entity
       end
+
       @category.parent_category = parent_category
+
     end
     @category.update(category_params)
     render json: @category, status: :ok
@@ -48,4 +56,5 @@ class CategoriesController < ApplicationController
     def set_category
       @category = Category.find(params[:id])
     end
+
 end
