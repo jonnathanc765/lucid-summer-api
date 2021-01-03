2977c2e84466ecc59670ba168d1c21bcf85fdbaa
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2021-01-03 19:09:06 -0400

    fix order checklist for frontend

--- app/controllers/checklist_controller.rb
+++ app/controllers/checklist_controller.rb
@@ -2,13 +2,28 @@ class ChecklistController < ApplicationController
 
   def check
 
-      @order_line = OrderLine.find(params[:id]) 
-      @order_line.update!(check: true)
-      if @order_line.order.status == "pending"
-        @order_line.order.status = "on_process" 
-        @order_line.order.save!
-      end
+    @order_line = OrderLine.preload(:product).find(params[:id]) 
+    @order_line.update!(check: true)
 
-      render json: @order_line, status: :ok
+    if @order_line.order.status == "pending"
+      @order_line.order.status = "on_process" 
+      @order_line.order.save!
+    end
+
+    # for return product with thumbnail
+
+    product = @order_line.product
+              
+    product = product.as_json.merge(
+      images: product.images.map { 
+        |image| { id: image.id, url: url_for(image) } 
+      }
+    )
+
+    order_line = @order_line.as_json.merge(
+      product: product
+    )
+
+    render json: order_line, status: :ok
   end
 end
